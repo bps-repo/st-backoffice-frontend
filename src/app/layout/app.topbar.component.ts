7214a39f8966cf2ec3d6395237a426f410af981b
heldersantiago273 <heldersantiago273@gmail.com>
2024-12-15 20:31:06 +0100

    change menu labels and toggle bar behavior

--- src/app/layout/app.topbar.component.ts
+++ src/app/layout/app.topbar.component.ts
@@ -1,15 +1,38 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
-import { LayoutService } from 'src/app/layout/service/app.layout.service';
+import {
+    LayoutService,
+    MenuMode,
+} from 'src/app/layout/service/app.layout.service';
+import { MenuService } from './app.menu.service';
 
 @Component({
     selector: 'app-topbar',
-    templateUrl: './app.topbar.component.html'
+    templateUrl: './app.topbar.component.html',
 })
 export class AppTopbarComponent {
-
     @ViewChild('menubutton') menuButton!: ElementRef;
 
-    constructor(public layoutService: LayoutService) { }
+    constructor(
+        public layoutService: LayoutService,
+        public menuService: MenuService
+    ) {}
+
+    get menuMode(): MenuMode {
+        return this.layoutService.config().menuMode;
+    }
+    set menuMode(_val: MenuMode) {
+        this.layoutService.config.update((config) => ({
+            ...config,
+            menuMode: _val,
+        }));
+        if (
+            this.layoutService.isSlimPlus() ||
+            this.layoutService.isSlim() ||
+            this.layoutService.isHorizontal()
+        ) {
+            this.menuService.reset();
+        }
+    }
 
     onMenuButtonClick() {
         this.layoutService.onMenuToggle();
@@ -21,5 +44,4 @@ export class AppTopbarComponent {
     onConfigButtonClick() {
         this.layoutService.showConfigSidebar();
     }
-    
 }
