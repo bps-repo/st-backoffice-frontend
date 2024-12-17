017c3908b2e5fa7d957d26bc16f269c737035c22
heldersantiago273 <heldersantiago273@gmail.com>
2024-12-17 14:40:14 +0100

    :sparkles: add students features

--- src/app/modules/schoolar/features/students/components/list/list.component.ts
+++ src/app/modules/schoolar/features/students/components/list/list.component.ts
@@ -1,12 +1,126 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ButtonModule } from 'primeng/button';
+import { DropdownModule } from 'primeng/dropdown';
+import { InputTextModule } from 'primeng/inputtext';
+import { MultiSelectModule } from 'primeng/multiselect';
+import { ProgressBarModule } from 'primeng/progressbar';
+import { RatingModule } from 'primeng/rating';
+import { RippleModule } from 'primeng/ripple';
+import { SliderModule } from 'primeng/slider';
+import { Table, TableModule } from 'primeng/table';
+import { ToastModule } from 'primeng/toast';
+import { ToggleButtonModule } from 'primeng/togglebutton';
+import { Customer, Representative } from 'src/app/demo/api/customer';
+import { CustomerService } from 'src/app/demo/service/customer.service';
+import { ProductService } from 'src/app/demo/service/product.service';
+import { CommonModule } from '@angular/common';
+import { FileUploadModule } from 'primeng/fileupload';
+import { ToolbarModule } from 'primeng/toolbar';
+import { SelectItem } from 'primeng/api/selectitem';
 
 @Component({
-  selector: 'app-list',
-  standalone: true,
-  imports: [],
-  templateUrl: './list.component.html',
-  styleUrl: './list.component.scss'
+    selector: 'app-list',
+    standalone: true,
+    imports: [
+        CommonModule,
+        FormsModule,
+        TableModule,
+        RatingModule,
+        ButtonModule,
+        SliderModule,
+        InputTextModule,
+        ToggleButtonModule,
+        RippleModule,
+        MultiSelectModule,
+        DropdownModule,
+        ProgressBarModule,
+        ToastModule,
+        FileUploadModule,
+        ToolbarModule,
+    ],
+    templateUrl: './list.component.html',
+    styleUrl: './list.component.scss',
 })
-export class ListComponent {
+export class ListComponent implements OnInit {
+    customers1: Customer[] = [];
+    loading: boolean = true;
+    statuses: any[] = [];
+    representatives: Representative[] = [];
+    activityValues: number[] = [0, 100];
+    cities: SelectItem[] = [];
+    selectedList: SelectItem = { value: '' };
 
+    selectedDrop: SelectItem = { value: '' };
+    @ViewChild('filter') filter!: ElementRef;
+
+    constructor(
+        private customerService: CustomerService,
+        private productService: ProductService
+    ) {}
+
+    ngOnInit(): void {
+        this.customerService.getCustomersLarge().then((customers) => {
+            this.customers1 = customers;
+            this.loading = false;
+
+            this.customers1.forEach(
+                (customer) =>
+                    (customer.date = new Date(
+                        customer.date as string
+                    ).toISOString())
+            );
+        });
+
+        this.representatives = [
+            { name: 'Amy Elsner', image: 'amyelsner.png' },
+            { name: 'Anna Fali', image: 'annafali.png' },
+            { name: 'Asiya Javayant', image: 'asiyajavayant.png' },
+            { name: 'Bernardo Dominic', image: 'bernardodominic.png' },
+            { name: 'Elwin Sharvill', image: 'elwinsharvill.png' },
+            { name: 'Ioni Bowcher', image: 'ionibowcher.png' },
+            { name: 'Ivan Magalhaes', image: 'ivanmagalhaes.png' },
+            { name: 'Onyama Limba', image: 'onyamalimba.png' },
+            { name: 'Stephen Shaw', image: 'stephenshaw.png' },
+            { name: 'XuXue Feng', image: 'xuxuefeng.png' },
+        ];
+
+        this.statuses = [
+            { label: 'Unqualified', value: 'unqualified' },
+            { label: 'Qualified', value: 'qualified' },
+            { label: 'New', value: 'new' },
+            { label: 'Negotiation', value: 'negotiation' },
+            { label: 'Renewal', value: 'renewal' },
+            { label: 'Proposal', value: 'proposal' },
+        ];
+
+        this.cities = [
+            {
+                label: 'Cidade',
+                value: { id: 1, name: 'New York', code: 'NY' },
+            },
+            { label: 'Centro', value: { id: 2, name: 'Rome', code: 'RM' } },
+            {
+                label: 'Maculusso',
+                value: { id: 3, name: 'London', code: 'LDN' },
+            },
+            {
+                label: 'Nova Vida',
+                value: { id: 4, name: 'Istanbul', code: 'IST' },
+            },
+            { label: 'Patriota', value: { id: 5, name: 'Paris', code: 'PRS' } },
+        ];
+    }
+
+    onGlobalFilter(table: Table, event: Event) {
+        table.filterGlobal(
+            (event.target as HTMLInputElement).value,
+            'contains'
+        );
+    }
+
+    clear(table: Table) {
+        table.clear();
+        this.filter.nativeElement.value = '';
+    }
 }
