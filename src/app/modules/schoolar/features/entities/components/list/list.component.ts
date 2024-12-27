9c3ecc457b8e7668136565cb41acc7bc2bb1efe9
heldersantiago273 <heldersantiago273@gmail.com>
2024-12-27 03:21:19 +0100

    Finish entities table

--- src/app/modules/schoolar/features/entities/components/list/list.component.ts
+++ src/app/modules/schoolar/features/entities/components/list/list.component.ts
@@ -1,40 +1,27 @@
 import { Component } from '@angular/core';
+import { Company } from 'src/app/core/models/company';
 import { TableStudentsEntityComponent } from 'src/app/shared/components/table-students-entity/table-students-entity.component';
+import { TableWithFiltersComponent } from 'src/app/shared/components/table-with-filters/table-with-filters.component';
+import { ENTITIES } from 'src/app/shared/constants/app';
+import { COMPANIES } from 'src/app/shared/constants/companies';
 
 @Component({
     selector: 'app-list',
     standalone: true,
-    imports: [TableStudentsEntityComponent],
+    imports: [TableWithFiltersComponent],
     templateUrl: './list.component.html',
     styleUrl: './list.component.scss',
 })
 export class ListComponent {
-    tableLabel = 'Customers';
+    tableLabel = 'Entidades';
     columns = [
+        { field: 'id', header: 'Número' },
         { field: 'name', header: 'Nome' },
-        { field: 'email', header: 'E-mail' },
         { field: 'phone', header: 'Telefone' },
         { field: 'address', header: 'Endereço' },
-        { field: 'status', header: 'Status' },
+        { field: 'students', header: 'Estudantes' },
     ];
-    data = [
-        {
-            name: 'John Doe',
-            email: 'johndoe@example.com',
-            phone: '123-456-7890',
-            address: '123 Main St',
-            status: 'Active',
-        },
-        {
-            name: 'Jane Smith',
-            email: 'janesmith@example.com',
-            phone: '987-654-3210',
-            address: '456 Elm St',
-            status: 'Inactive',
-        },
-        // Add more customers here...
-    ];
-    loading = false;
-    globalFilterFields = ['name', 'email', 'phone', 'address', 'status'];
-    entity = 'customer';
+    entities: Company[] = COMPANIES;
+
+    globalFilterFields: string[] = [];
 }
