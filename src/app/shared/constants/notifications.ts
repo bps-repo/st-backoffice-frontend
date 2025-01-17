c47e657416eb2615d1a7391f94028e81e78f20f4
heldersantiago273 <heldersantiago273@gmail.com>
2025-01-17 16:59:54 +0100

    add notifications.ts constant

---
+++ src/app/shared/constants/notifications.ts
@@ -0,0 +1,22 @@
+export const NOTIFICATIONS = [
+    {
+        title: 'Aviso 1',
+        message: 'Terminou o quiz n#03-adjectives...',
+        icon: 'pi pi-wallet',
+    },
+    {
+        title: 'Aviso 2',
+        message: 'Acabou de agendar uma aula para as 12h',
+        icon: 'pi pi-bell',
+    },
+    {
+        title: 'Aviso 3',
+        message: 'Fez abertura de um inquerito para as 12h',
+        icon: 'pi pi-file-pdf',
+    },
+    {
+        title: 'Aviso 4',
+        message: 'Pagamento do certificado Beginner',
+        icon: 'pi pi-money-bill',
+    },
+];
