7d4cc8315ca56f69f4e769851f0f58786112f097
heldersantiago273 <heldersantiago273@gmail.com>
2025-01-23 11:07:30 +0100

    add materials enum

---
+++ src/app/core/enums/material.ts
@@ -0,0 +1,21 @@
+export enum MaterialType {
+    Text = 'texto',
+    Video = 'video',
+    Audio = 'audio',
+    Document = 'documento',
+    Image = 'imagem',
+    PDF = 'pdf',
+    Excel = 'excel',
+    Word = 'word',
+    PowerPoint = 'powerpoint',
+    Zip = 'zip',
+    RAR = 'rar',
+    TAR = 'tar',
+    ISO = 'iso',
+    Other = 'outros',
+}
+
+export enum MaterialStatus {
+    Active = 'ativo',
+    Inactive = 'inativo',
+}
