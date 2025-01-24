4253fff1f1696de5774704a6009db4f351c5ddc8
heldersantiago273 <heldersantiago273@gmail.com>
2025-01-24 11:16:22 +0100

    Add invoice interface and apply a Partial<Invoice>

--- src/app/core/models/invoice.ts
+++ src/app/core/models/invoice.ts
@@ -1,1 +1,27 @@
-export interface Invoice {}
+export interface Invoice {
+    id: number;
+    code: string;
+    price: number;
+    date: string;
+    dueDate: string;
+    customer?: {
+        id: number;
+        name: string;
+    };
+    status: string;
+    products?: {};
+    totalItems: number;
+    totalValue: number;
+    totalDiscount: number;
+    totalTaxes: number;
+    total: number;
+    paymentMethod: string;
+    paymentStatus: string;
+    paymentDate: string;
+    shippingAddress: {};
+    billingAddress: {};
+    shippingCost: number;
+    shippingMethod: string;
+    shippingDate: string;
+    shippingStatus: string;
+}
--- src/app/modules/schoolar/features/students/components/detail/tabs/invoices/invoices.component.ts
+++ src/app/modules/schoolar/features/students/components/detail/tabs/invoices/invoices.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
+import { Invoice } from 'src/app/core/models/invoice';
 
 @Component({
-  selector: 'app-invoices',
-  standalone: true,
-  imports: [],
-  templateUrl: './invoices.component.html',
-  styleUrl: './invoices.component.scss'
+    selector: 'app-invoices',
+    standalone: true,
+    imports: [],
+    templateUrl: './invoices.component.html',
+    styleUrl: './invoices.component.scss',
 })
 export class InvoicesComponent {
+    columns: any[] = [];
 
+    invoices: Partial<Invoice>[] = [];
+
+    constructor() {}
+
+    ngOnInit(): void {
+        this.columns = [
+            { field: 'code', header: 'Factura' },
+            { field: 'name', header: 'Valor' },
+            { field: 'price', header: 'Preço' },
+            { field: 'date', header: 'Data de Emissão' },
+            { field: 'date', header: 'Data de Vencimento' },
+            { field: 'status', header: 'Estado' },
+        ];
+    }
 }
