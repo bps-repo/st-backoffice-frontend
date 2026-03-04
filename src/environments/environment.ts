168a8188cf5dc5110e502ff1ae3e0ace93b699f7
helder.santiago <helder.santiago@bancobai.ao>
2026-03-04 12:18:01 +0100

    feat: update API endpoint

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,5 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://localhost:8080/api/v1',
-    apiUrl2: 'http://45.55.87.103/api/v1',
+    apiUrl: 'http://45.55.87.103/api/v1',
     enableHealthChecks: false,
 };
