b3f0dfebcac778b6528c4272c3117e6721c17531
helder.santiago <helder.santiago@bancobai.ao>
2026-04-02 20:04:22 +0100

    refactor: improve error handling and code readability in `ShowToastErrorService`
    - Updated method signature to include return type for better clarity.
    - Simplified null and undefined checks for error handling.
    - Enhanced readability by restructuring conditional statements and standardizing formatting.

--- src/app/shared/services/show-toast-error-service.ts
+++ src/app/shared/services/show-toast-error-service.ts
@@ -1,25 +1,27 @@
-import {ApiError} from "../../core/models/ApiError";
-import {MessageService} from "primeng/api";
+import { ApiError } from "../../core/models/ApiError";
+import { MessageService } from "primeng/api";
 
 export class ShowToastErrorService {
 
-    static showToastError(title: string, error: Partial<ApiError> | null, messageService: MessageService) {
-        if (error && error.validationErrors && error.validationErrors.length > 0) {
+    static showToastError(title: string, error: Partial<ApiError> | null, messageService: MessageService): void {
+        if (!error) return;
+
+        if (error.validationErrors?.length) {
             error.validationErrors.forEach(e => {
                 messageService.add({
                     life: 5000,
                     severity: 'error',
                     summary: `${error.message} - ${e.field}`,
                     detail: e.message
-                })
+                });
             });
-        } else if (error && error.message) {
+        } else if (error.message) {
             messageService.add({
                 life: 5000,
                 severity: 'error',
                 summary: title,
-                detail: `${error?.message}`
-            })
+                detail: error.message
+            });
         }
     }
 }
