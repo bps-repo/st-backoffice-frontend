309383ede58ce5863c72ee2b247284c122d73298
helder.santiago <helder.santiago@bancobai.ao>
2026-06-05 00:14:51 +0100

    chore: remove assessments module as part of project restructuring
    - Deleted the AssessmentsModule file to streamline the application structure and eliminate unused components.

--- src/app/features/schoolar/features/assessments/assessments.module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { AssessmentsRoutes } from './assessments.routes';
-
-@NgModule({
-    imports: [CommonModule, AssessmentsRoutes],
-    declarations: [],
-})
-export class AssessmentsModule {}
